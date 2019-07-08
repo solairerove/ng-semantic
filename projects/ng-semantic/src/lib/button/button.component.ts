3fe1bfcb73cf3985695258d91936be65ccec565d
Mikita Kryvitski <solairerove@gmail.com>
2019-07-08 19:10:08 +0300

    feature: add click handling to button

--- projects/ng-semantic/src/lib/button/button.component.ts
+++ projects/ng-semantic/src/lib/button/button.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ChangeDetectionStrategy
+} from '@angular/core';
 
 @Component({
   selector: 'ui-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent implements OnInit {
   @Input() title: string;
 
+  @Output() clickEmitter = new EventEmitter<any>();
+
   constructor() {}
 
   ngOnInit() {}
+
+  clickEmit() {
+    this.clickEmitter.emit();
+  }
 }
